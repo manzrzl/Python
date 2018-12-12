3a0ae6beb88dfda0e623d0a788ce3ae5d55265ca
manzrzl <maanojriijal@gmail.com>
2018-12-12 12:49:30 +0545

    variables in python

---
+++ variables.py
@@ -0,0 +1,20 @@
+# x = 1 # int 
+# y = 2.5 #float
+# name = 'Manoj' #str
+# is_cool = True #bool
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, 'Manoj',True)
+
+# Basic Math
+a = x + y
+
+# Casting
+x = str(x)
+y = int(y)
+z = float(y)
+
+
+# print(x,y,name,is_cool)
+
+print(type(z),z)
