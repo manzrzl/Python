08b929693a2f1cdfc45a8e679bcd8fc846f1f0f3
Manoj Rijal <maanojriijal@gmail.com>
2018-12-25 14:07:41 +0545

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,63 @@
+#Arithmetic Operators
+'''
+num1 = 20
+num2 = 30
+print("num1 + num2 =", num1+num2)
+print("num1 - num2 =", num1-num2)
+print("num1 * num2 =", num1*num2)
+print("num1 / num2 =", num1/num2)
+print('5^3 =', 5**3)
+print("20 % 3 =", 20%3)
+print("22//7=", 22//7)
+'''
+#Assignment Operator
+'''
+num3 = num1 + num2
+print(num3)
+num3+=num2
+print(num3)
+'''
+#num3 = num3 + num2
+
+
+#Comparision Operator
+'''
+num1 = 20
+num2 = 30
+num3 = 80
+print("Is num3 > num2 ?",num3 > 2)
+print("Is num2 = num3 ?",num2 == num3)
+print("Is num1 != num2 ?",num1 != num2)
+'''
+
+#Logical Operator
+'''
+x = True
+y = False
+
+print("X and Y", x and y)
+print("X or Y", x or y)
+print("not of x", not x)
+'''
+
+
+#Bitwise Operator
+num4 = 6 #110
+num5 = 2 #010
+'''
+print('Bitwise and =', num4 & num5)
+print('Bitwise or =', num4 | num5)
+print('Bitwise xor =', num4 ^ num5)
+print("Num4 rightshift by 2", num4 >> 2)
+print("Num5 leftshift by 2", num5 << 2)
+'''
+
+
+#Identity Operator
+    #is/isnot
+#Membership Operator
+    #in/not in
+
+    
+
+
